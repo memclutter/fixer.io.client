092c849031c6adfb7859ca53e3434934305d6597
Memory Clutter <memclutter@gmail.com>
2018-03-01 01:10:18 +0300

    Command line arguments

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
 )
@@ -14,8 +15,32 @@ type Result struct {
 	Rates map[string]float64 `json:"rates"`
 }
 
+var (
+	period  string
+	base    string
+	symbols string
+)
+
+func init() {
+	flag.StringVar(&period, "period", "latest", "Get historical rates for any day since 1999. Default latest")
+	flag.StringVar(&base, "base", "EUR", "Rates are quoted against the Euro by default. ")
+	flag.StringVar(&symbols, "symbols", "", "Request specific exchange rates by setting the symbols parameter.")
+	flag.Parse()
+}
+
 func main() {
-	resp, err := http.Get(BaseUrl + "latest")
+	req, err := http.NewRequest("GET", BaseUrl+period, nil)
+	if err != nil {
+		log.Panicf("Create GET-request error: %v", err)
+	}
+
+	q := req.URL.Query()
+	q.Add("base", base)
+	q.Add("symbols", symbols)
+
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := http.Get(req.URL.String())
 	if err != nil {
 		log.Panicf("Execute GET-request error: %v", err)
 	}
